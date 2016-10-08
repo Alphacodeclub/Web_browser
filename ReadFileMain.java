73c519c8e810c1e83c43b328ced5c3e366ee3d4f
rootanujraval24 <Anujraval24@gmail.com>
2016-10-09 00:50:02 +0530

    Add files via upload

---
+++ ReadFileMain.java
@@ -0,0 +1,11 @@
+package com.browser;
+import javax.swing.JFrame;
+public class ReadFileMain {
+
+	public static void main(String[] args) {
+	
+		Read_file rf = new Read_file();
+		rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
